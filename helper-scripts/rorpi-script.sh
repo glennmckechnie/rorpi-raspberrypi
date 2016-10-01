587c3d8cf2b534d4e4ef037aa278ebf3e05d18d1
Glenn <graibeard@gmail.com>
2016-10-01 11:51:55 +1000

    Bump version to 0.1.2
    check if running on a raspberry pi, also add "run once only" check,

--- helper-scripts/rorpi-script.sh
+++ helper-scripts/rorpi-script.sh
@@ -7,7 +7,11 @@
 #       If we execute it more than once, then we should verify the content of: '.bash_logout' and '.bashrc' for root and pi user.
 #         
 # Version
-#  0.1.1: initial version
+#  0.1.2: initial version
+
+rorpitar=rorpi-ro-setup.0.0.3.tar.gz
+TEMP=""
+FOLDER=""
 
 #===============================================================================
 # Function
@@ -30,16 +34,45 @@ backup_file () {
 #===============================================================================
 #MAIN
 #===============================================================================
-TEMP=""
-FOLDER=""
 STRING="Script begin..."
 echo $STRING
+
+# check if we are running on a raspberry pi, if so assume
+# it's okay to continue, if not Panic!
+
+grep /boot/issue.txt -e Raspberry > /dev/null 2>&1
+if (( $? != "0" ))
+then
+ echo "Not a raspberry pi, aborting script."
+ exit 1 # exit with error
+fi
 
 # This script must be executed under root (ie: 'sudo -i')
 # Make sure only root can run our script
 if [ "$(id -u)" != "0" ]; then
    echo "This script must be run as root" 1>&2
-   exit 1
+   exit 1 # exit with error
+fi
+
+cd /root
+
+# Check for existence of file (see script end) and abort if present
+if [  -f /root/rorpi-ro-setup/done-once-already ]
+then
+    echo " Aborting script as it has already been run. "
+    exit 1 # exit with error
+fi
+
+# We've passed all tests, we can continue
+
+#Download 'rorpi-ro-setup-x.x.x.x.tar.gz' in the /root folder.
+if [ -f $rorpitar ];
+then
+    echo "SKIP. (File $rorpitar exists)"
+else
+    echo "WGET. (File $rorpitar does not exist)"
+    wget https://github.com/glennmckechnie/rorpi-raspberrypi/raw/master/$rorpitar --no-check-certificate
+    tar -zxf $rorpitar
 fi
 
 #Change directory to root
@@ -58,17 +91,6 @@ backup_file '/etc/default/tmpfs'
 backup_file '/etc/init.d/checkroot-bootclean.sh'
 #           '/etc/init.d/voyage-sync' (not present)
 
-#Download 'rorpi-ro-setup-x.x.x.x.tar.gz' in the /root folder.
-TEMP=rorpi-ro-setup.0.0.2.0.tar.gz
-cd /root
-if [ -f $TEMP ];
-then
-    echo "SKIP. (File $TEMP exists)"
-else
-    echo "WGET. (File $TEMP does not exist)"
-    wget https://github.com/glennmckechnie/rorpi-raspberrypi/raw/master/rorpi-ro-setup.0.0.2.0.tar.gz --no-check-certificate
-    tar -zxf rorpi-ro-setup.0.0.2.0.tar.gz
-fi
 
 #Create directories
 mkdir /etc/lighttpd
@@ -86,7 +108,7 @@ mkdir /ro/var/lib/logrotate
 mkdir /ro/var/lib/ntp
 mkdir /ro/var/spool
 mkdir /ro/var/spool/cron
-mkdir /ro/var/spool/cron/crontabs
+#mkdir /ro/var/spool/cron/crontabs # copy this instead
 mkdir /ro/var/spool/rsyslog
 mkdir /ro/var/www
 mkdir /ro/var/www/html
@@ -122,14 +144,16 @@ cp ./$FOLDER/etc/fstab.rorpi /etc/fstab
 cp ./$FOLDER/etc/inittab.rorpi /etc/inittab
 cp ./$FOLDER/etc/motd.rorpi /etc/motd
 cp ./$FOLDER/etc/ntp.conf.rorpi /etc/ntp.conf
-cp ./$FOLDER/etc/resolv.conf.rorpi /run/resolv.conf            # which one is it ??
-cp ./$FOLDER/etc/resolv.conf.rorpi /run/resolvconf/resolv.conf # which one is it ??
+#cp ./$FOLDER/etc/resolv.conf.rorpi /run/resolv.conf            # which one is it ??
+#cp ./$FOLDER/etc/resolv.conf.rorpi /run/resolvconf/resolv.conf # which one is it ??
+# probably neither
 cp ./$FOLDER/etc/default/tmpfs.rorpi /etc/default/tmpfs
 cp ./$FOLDER/etc/default/voyage-util /etc/default/voyage-util
 cp ./$FOLDER/etc/init.d/checkroot-bootclean.sh.rorpi /etc/init.d/checkroot-bootclean.sh
 cp ./$FOLDER/etc/init.d/voyage-sync /etc/init.d/voyage-sync
 cp ./$FOLDER/etc/lighttpd/lighttpd.conf.rorpi /etc/lighttpd/lighttpd.conf
-#cp ./$FOLDER/etc/resolvconf/run /etc/resolvconf/run  # this is a software link. see below.
+cp -Rp ./$FOLDER/ro/var/spool/cron/crontabs /ro/var/spool/cron/crontabs # copied rather than created
+
 
 #Create link1 (soft?)
 rm /etc/resolv.conf
@@ -140,35 +164,28 @@ cd /etc/resolvconf
 ln -sf /run/resolvconf run
 cd /root
 
-#tosee:alternative?
-#cd /etc/resolvconf
-#ls -sf /run/resolv.conf run
-
 #Copy the other files to folder
 cp ./$FOLDER/lib/voyage-utils/100-rpi /lib/voyage-utils/
 
-#Create stub
-#touch /ro/var/cache/unbound/empty-stub
-#touch /ro/var/lib/dhcp/empty-stub
-#touch /ro/var/lib/dhcpcd5/empty-stub
-#touch /ro/var/lib/logrotate/empty-stub
-#touch /ro/var/lib/ntp/empty-stub
-#touch /ro/var/spool/cron/crontabs/empty-stub
-#touch /ro/var/spool/rsyslog/empty-stub
-#touch /ro/var/www/html/weewx/empty-stub
-
 #bash
-#Warning: Execute script only once.
-STRING="Note:We shoud append text only once to .bash_logout and .bashrc"
-echo $STRING
 
-cat ./$FOLDER/root/.bash_logout.rorpi >> /root/.bash_logout     #append only once.
-cat ./$FOLDER/root/.bash_logout.rorpi >> /etc/skel/.bash_logout #append only once
-cat ./$FOLDER/root/.bashrc-append.rorpi >> /root/.bashrc        #append only once?
-cat ./$FOLDER/root/.bashrc-append.rorpi >> /etc/skel/.bashrc    #append only once?
+if [ -f "./$FOLDER/root/.bashrc-append.rorpi.done" ]
+then 
+  #Warning: Execute script only once.
+  STRING="Note:We shoud append text only once to .bash_logout and .bashrc"
+  echo $STRING
+else
+  cat ./$FOLDER/root/.bash_logout.rorpi >> /root/.bash_logout     #append only once.
+  cat ./$FOLDER/root/.bash_logout.rorpi >> /etc/skel/.bash_logout #append only once
+  cat ./$FOLDER/root/.bashrc-append.rorpi >> /root/.bashrc        #append only once?
+  cat ./$FOLDER/root/.bashrc-append.rorpi >> /etc/skel/.bashrc    #append only once?
 
-cat ./$FOLDER/root/.bash_logout.rorpi >> /home/pi/.bash_logout
-cat ./$FOLDER/root/.bashrc-append.rorpi >> /home/pi/.bashrc #append only once?
+  cat ./$FOLDER/root/.bash_logout.rorpi >> /home/pi/.bash_logout
+  cat ./$FOLDER/root/.bashrc-append.rorpi >> /home/pi/.bashrc #append only once?
+
+  mv ./$FOLDER/root/.bash_logout.rorpi ./$FOLDER/root/.bash_logout.rorpi.done
+  mv ./$FOLDER/root/.bashrc-append.rorpi ./$FOLDER/root/.bashrc-append.rorpi.done
+fi
 
 #Helper scripts to remount
 cp ./$FOLDER/usr/local/sbin/fastreboot /usr/local/sbin/fastreboot
@@ -176,8 +193,9 @@ cp ./$FOLDER/usr/local/sbin/remountro /usr/local/sbin/remountro
 cp ./$FOLDER/usr/local/sbin/remountrw /usr/local/sbin/remountrw
 cp ./$FOLDER/usr/local/sbin/remove.docs /usr/local/sbin/remove.docs
 cd /usr/local/sbin
-ln -s remountro rero  # create softlinks as it makes it obvious they are duplicate files
-ln -s remountrw rerw
+ln -sf remountro rero  # create softlinks as it makes it obvious they are duplicate files
+ln -sf remountrw rerw
+
 cd /root
 
 #Symlink to tmp
@@ -191,8 +209,8 @@ rm -rf /var/spool/rsyslog && ln -sf /tmp/var/spool/rsyslog /var/spool/rsyslog
 rm -rf /var/www/html && ln -sf /tmp/var/www/html /var/www/html
 
 #Give proper ownership
-chown pi:pi /ro/home/pi/.bash_logout
-chown pi:pi /ro/home/pi/.bashrc
+#chown pi:pi /home/pi/.bash_logout # probably redundant, removing to check
+#chown pi:pi home/pi/.bashrc
 cd /var/lib
 chown ntp.ntp ntp
 
@@ -200,12 +218,14 @@ chown ntp.ntp ntp
 update-rc.d voyage-sync defaults
 update-rc.d  dphys-swapfile disable
 
+touch /root/rorpi-ro-setup/done-once-already
+
 STRING="Script end."
 echo $STRING
 
 STRING="You can now reboot the RPI with 'reboot -n'"
 echo $STRING
 
-exit 1
+exit 0 # exited successfully
 
 #END
