fa760bb4ccf818da90d842b0fc25c1310e45c3ef
Glenn <glenn.mckechnie@users.noreply.github.com>
2017-03-05 22:51:19 +1100

    colorize for readability

--- helper-scripts/rorpi-preinstall.sh
+++ helper-scripts/rorpi-preinstall.sh
@@ -15,11 +15,18 @@
 # 0.11: Use the weewx apt repository.
 # 0.12: Add an option to retain systemd. !! Work in progress - breakages may occur !!
 # 0.13: Add RTC notes
+# 0.14: Colorize - all the better to see the script messages grandma...
 #
-# Version 0.13
+# Version 0.14
 
 # apt-get provides an option to say yes to installation targets by using the -y string
 # set this as required, ie:- apt_optn="-y"  will allow it to be done automatically
+
+red='\e[0;31m'
+yellow='\e[0;33m'
+cyan='\e[0;36m'
+norm='\e[0m'
+
 apt_optn=""
 
 # Currently, the default initmethod is to remove systemd, adjust the following to suit your needs.
@@ -27,12 +34,13 @@ apt_optn=""
 #initmethod="systemd" 
 initmethod="sysv"
 
-echo -e "\nIf you do not wish to install weewx with this script then add 'noweewx'"
-echo -e "\n ie:- $0 noweewx\n"
+echo -e "\n$yellow If you do not wish to install weewx with this script then add 'noweewx'"
+echo -e "\n$cyan                  ie:- $0 noweewx $norm\n"
 sleep 2
-echo -e "\nIf you want to retain systemd, and you're willing to test the result."
-echo -e "\nThen edit  the 'initmethod' variable within this script"
-echo -e "\nie:uncomment #initmethod=systemd"
+echo -e "\n$yellow If you want to retain systemd, and you're willing to test the result."
+echo -e "\n Then edit the 'initmethod' variable within this script"
+echo -e "\n          ie: uncomment #initmethod=systemd"
+echo -e "\n Feedback on the success or otherwise would be appreciated. $norm"
 sleep 4
 
 check_version()
@@ -49,15 +57,16 @@ check_version()
    if [[ "$winner" < $hubversion ]]
    then
      cp /tmp/"$tempfile" /root/rorpi-preinstall."$hubversion".sh
-     echo " "An updated rorpi-preinstall.sh "(Version $hubversion)" is available on github.
-     echo " "It is also available here at /root/rorpi-preinstall-newest.sh
-     echo "      "bash /root/rorpi-preinstall."$hubversion".sh
-     echo " "It is strongly suggested to use this newer version instead.
+     echo -e "$yellow An updated rorpi-preinstall.sh (Version $hubversion) is available on github."
+     echo -e " It is also available here at /root/rorpi-preinstall-newest.sh"
+     echo -e "$cyan      bash /root/rorpi-preinstall.$hubversion.sh"
+     echo -e "$yellow It is strongly suggested to use this newer version instead. $norm"
    exit 0
    else
-     echo "script version on github is $hubversion"
-     echo "This version is $thisversion so doesn't need updating"
-     echo "This script will continue in 6 seconds (Ctrl-C to abort)"
+     echo -e "$yellow  The script version on github is $hubversion"
+     echo -e " This version is $thisversion so doesn't need updating"
+     echo -e "                Okay to continue"
+     echo -e " This script will continue in 6 seconds $cyan (Ctrl-C to abort) $norm"
      sleep 6
      return 0
    fi
@@ -79,40 +88,43 @@ then
 # for apt-get, also do it only once
   raspi-config
 
- echo -e "\n\tRunning apt-get update and dist-upgrade\n"
+ echo -e "\n\t$yellow Running apt-get update and dist-upgrade \n"
+ echo -e "\n\t         This may take awhile... $norm\n"
  apt-get update
  apt-get $apt_optn dist-upgrade
 
 case $initmethod in
         sysv)
-         echo -e "\n\tInstalling sysvinit"
+         echo -e "\n\t$yellow Installing sysvinit $norm"
 
          apt-get $apt_optn install sysvinit-core sysvinit-utils
 
-         echo -e "\n\tWe need to reboot to remove systemd"
-         echo -e "\n\tWaiting 6 seconds before rebooting"
+         echo -e "\n\t$yellow We need to reboot to remove systemd $norm"
+         echo -e "\n\t$red Waiting 10 seconds before rebooting $norm"
          sleep 2
-         echo -e "\n\n\tRun this script again after the reboot\n\n"
+         echo -e "\n\n\t$yellow Run this script again after the reboot.\n\n"
          touch /root/initmethod
+         sleep 4
+         echo -e "\n\n\tRepeating: Run this script again after the reboot $norm\n\n"
          sleep 4
 
          reboot
          exit 1
         ;;
         systemd)
-         echo -e "\nRetaining systemd as our init method"
-         echo -e "\nnb:- This action is a work in progress"
+         echo -e "\n$yellow Retaining systemd as our init method "
+         echo -e "\n$red nb:- This action is a work in progress $norm"
          ;;
 esac
 else
- echo -e "\n\tContinuing installation, updating  process\n"
+ echo -e "\n\t$yellow Continuing the installation, or updating process $norm\n"
 fi
 
 # possibly hitting this after the reboot required to cleanly remove sysV
 
 cd /root # and we still should be there already - but...
  rm -f /root/initmethod
- 
+
  case $initmethod in
         sysv)
         apt-get $apt_optn remove --purge --auto-remove systemd
@@ -131,7 +143,7 @@ apt-get $apt_optn install lighttpd sqlite3 rsync mc lynx byobu bootlogd multitai
 # see https://github.com/glennmckechnie/rorpi-raspberrypi/wiki/rorpi-raspberrypi-preinstall#RTC_Optional
 apt-get install i2c-tools
 
-echo -e "\n\tPurging fake -hwclock\n"
+echo -e "\n\t$yellow Purging fake -hwclock $norm\n"
 apt-get $apt_optn purge fake-hwclock
 
 apt-get purge wolfram-engine desktop-base lightdm lxappearance lxde-common \
@@ -141,23 +153,23 @@ samba-libs squeak-plugins-scratch squeak-vm supercollider
 
 apt-get $apt_optn -f install # while that shouldn't be required.
 
-echo -e "\n\tRemoving apt-get targeted applications - autoremove\n"
+echo -e "\n\t$yellow Removing apt-get targeted applications - autoremove $norm\n"
 apt-get autoremove # this will
 
 case $1 in
         noweewx)
-        echo -e "\t\nNOT installing weewx\n"
+        echo -e "\t\n$cyan NOT installing weewx $norm\n"
         ;;
         *)
-           echo -e "\n\tSetting up the local apt repository with weewx\n"
+           echo -e "\n\t$yellow Setting up the local apt repository with weewx $norm\n"
            curl -s http://weewx.com/keys.html | sudo apt-key add -
            echo "deb [arch=all] http://weewx.com/apt/ squeeze main" | sudo tee /etc/apt/sources.list.d/weewx.list
 
-           echo -e "\n\tFetching and Installing weewx\n"
+           echo -e "\n\t$yellow Fetching and Installing weewx $norm\n"
            sudo apt-get update
            sudo apt-get install weewx
 
-           echo -e "\n\tCompleting weewx installation\n"
+           echo -e "\n\t$yellow Completing weewx installation $norm\n"
            # apt-get $apt_optn update # ignoring as we've only just done an update.
            apt-get $apt_optn -f install
 
@@ -169,7 +181,7 @@ esac
 # running mc now to set up directories and stop it complaining when we become read only
 mc
 
-echo -e "\n\tRaspbian installation has been updated. Also check your weewx installation is"
-echo -e "\tworking and then continue with the read only installation steps (or use the"
-echo -e "\trorpi-readonly.sh script)\n"
+echo -e "\n\t$yellow This Raspbian installation has been updated. Also check your weewx installation "
+echo -e "\t is working and then continue with the read only installation steps (or use the"
+echo -e "\t rorpi-readonly.sh script) $norm\n"
 
